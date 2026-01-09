f25f5aac730a2dad6620b8792acf29f2c5970777
D3terri0rr <maudosocrestani@icloud.com>
2026-01-08 23:38:18 -0300

    Update Noise.js

--- Noise.js
+++ Noise.js
@@ -1,3 +1,11 @@
+// Simplex/Perlin noise 1D pseudo-random
+let seed = Math.random() * 1000;
+
 export function noise(x){
-    return (Math.sin(x*12.9898) * 43758.5453) % 1;
+    return Math.sin(x * 12.9898 + seed) * 43758.5453 % 1;
+}
+
+// Para suavizar entre 0 e 1
+export function smoothNoise(x){
+    return noise(x) * 0.5 + 0.5;
 }
